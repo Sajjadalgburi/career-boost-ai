a014d37d100278c571defd849ac86389f2c4d73b
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-01-28 13:10:01 -0700

    Configure custom font families in Tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -60,6 +60,10 @@ export default {
         md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
       },
+      fontFamily: {
+        sans: ["var(--font-inter)"],
+        mono: ["var(--font-geist-mono)"],
+      },
     },
   },
   plugins: [tailwindcssAnimate],
