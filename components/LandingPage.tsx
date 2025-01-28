40c625da9119fbb377cf00402ad904ea7f3ed534
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-01-28 12:22:47 -0700

    Add LandingPage component with basic structure

---
+++ components/LandingPage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LandingPage = () => {
+  return (
+    <div className="max-w-5xl container w-full ">
+      <h1>Landing Page</h1>
+    </div>
+  );
+};
+
+export default LandingPage;
