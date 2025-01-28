c72af23e201573adfe1726a19bb9fbfc91eeb037
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-01-28 12:06:59 -0700

    Add Navbar component with authentication and theme toggle

---
+++ components/Navbar.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { ModeToggle } from "./ModeToggle";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+
+const Navbar = () => {
+  return (
+    <nav className="flex justify-between items-center p-4 w-full">
+      <p className="text-2xl font-bold p-2">CareerBoost AI</p>
+      <div className="flex items-center gap-2">
+        <ModeToggle />
+
+        <SignedIn>
+          <div className="auth-button">
+            <UserButton />
+          </div>
+        </SignedIn>
+
+        <SignedOut>
+          <div className="auth-button">
+            <SignInButton />
+          </div>
+        </SignedOut>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
