3f82d290055c270aefd422b2ef6328d1dfb4e6fb
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-01-29 15:08:25 -0700

    Add example prompts for career guidance and job search

--- helpers/index.ts
+++ helpers/index.ts
@@ -3,3 +3,29 @@ import { toast } from "react-hot-toast";
 export const setErrorToast = (error: string, duration = 3000) => {
   toast.error(error, { duration });
 };
+
+export const examples = [
+  {
+    title: "Help me find a job",
+  },
+  {
+    title: "Improve my resume",
+  },
+  {
+    title: "I want to work in a startup",
+  },
+
+  {
+    title: "Suggest career paths based on my skills",
+  },
+  {
+    title: "Help me negotiate a better salary",
+  },
+  {
+    title: "Find remote job opportunities",
+  },
+
+  {
+    title: "I want to transition into software development",
+  },
+];
