eaf71cdf3eaa60160f357ce6b9f5d63fa1cf300c
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-01-29 12:02:24 -0700

    Refactor Navbar component with improved responsiveness and navigation

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { ModeToggle } from "./ModeToggle";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <nav className="flex justify-between items-center p-3 mt-3 w-full max-w-5xl container bg-background dark:bg-background border border-border backdrop-blur-lg rounded-2xl sticky top-0 z-50">
-      <p className="text-2xl font-bold p-2">CareerBoost AI</p>
-      <div className="flex items-center gap-2">
+    <nav className="flex justify-between px-4 sm:px-8 items-center p-3 mt-3 w-full max-w-5xl container bg-background dark:bg-background border border-border backdrop-blur-lg rounded-2xl sticky top-0 z-50">
+      <Link href="/" className="text-2xl font-bold p-2">
+        CareerBoost AI
+      </Link>
+      <div className="flex items-center gap-5">
         <ModeToggle />
 
         <SignedIn>
