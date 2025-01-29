23bea1a35e3087ae6fbb4e906d0df4f25d83b941
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-01-29 16:30:47 -0700

    Redesign results page with AI resume improvement and job search sections

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,12 @@ import LandingPage from "@/components/LandingPage";
 import { setErrorToast } from "@/helpers";
 import React from "react";
 import toast, { Toaster } from "react-hot-toast";
+import Image from "next/image";
 
 const Page = () => {
-  const [showResults, setShowResults] = React.useState<boolean>(false);
+  // ! TESTING
+  const [showResults, setShowResults] = React.useState<boolean>(true); // ! TESTING
+  // ! TESTING
   const [resume, setResume] = React.useState<File | null>(null);
   const [resumeText, setResumeText] = React.useState<string>("");
   const [prompt, setPrompt] = React.useState<string>("");
@@ -97,7 +100,6 @@ const Page = () => {
        * 3a. Make API call to get chat response from AI
        * 3b. Scrape LinkedIn profile based on user resume
        */
-
       const res = await fetch("/api/improvedResume", {
         method: "POST",
         headers: {
@@ -146,8 +148,36 @@ const Page = () => {
           error={error}
         />
       ) : (
-        <div>
-          <p>{answer}</p>
+        <div className="w-full flex min-h-fit items-center  gap-3 px-3">
+          {/* Resume Improvement from AI */}
+          <div className="w-2/3 h-[750px] backdrop-blur-lg p-5 shadow-2xl rounded-xl border  border-white/65">
+            <div className="w-full backdrop-blur-lg p-4  flex items-start gap-2 border  border-white/65">
+              <Image src="globe.svg" alt="resume" width={25} height={25} />
+              <p className="dark:text-white text-black">{answer}</p>
+            </div>
+          </div>
+
+          <div className="w-1/3 h-[500px] flex flex-col">
+            {/* Copy Resume */}
+            <div className="w-full bg-black text-white p-5 rounded-xl">
+              <p>Copy Resume</p>
+            </div>
+            <div className=" flex backdrop-blur-lg p-5 shadow-xl rounded-xl border border-white/45">
+              <div>
+                <h3 className="text-2xl font-bold dark:text-white">
+                  Job Search
+                </h3>
+                <p className="dark:text-white">
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                  Quidem nulla distinctio commodi temporibus nisi accusamus
+                  laborum aperiam repudiandae facere tempore voluptates
+                  exercitationem eveniet atque magni officia, enim neque dolorem
+                  porro?
+                </p>
+              </div>
+            </div>
+          </div>
+          {/* Job Search Board */}
         </div>
       )}
     </section>
