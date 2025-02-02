5427ee790e74a47ffbf4f1aa6d758c5d257b5bd3
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-02-01 17:46:57 -0700

    Configure Edge runtime for job scraping API route

--- app/api/jobScrape/route.ts
+++ app/api/jobScrape/route.ts
@@ -1,5 +1,10 @@
 import { NextRequest, NextResponse } from "next/server";
 
+export const config = {
+  runtime: "edge",
+};
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export async function POST(req: NextRequest) {
   return NextResponse.json({ message: "Hello World" });
 }
