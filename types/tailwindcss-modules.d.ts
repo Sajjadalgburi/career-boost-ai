448ba60f31f5ddd55e20d5d3cbd048eac8964885
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-02-01 17:45:48 -0700

    Add TypeScript declaration for Tailwind's flattenColorPalette utility

---
+++ types/tailwindcss-modules.d.ts
@@ -0,0 +1,6 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+declare module "tailwindcss/lib/util/flattenColorPalette" {
+  // You can specify the actual types if known, or use `any` as a placeholder
+  const flattenColorPalette: (colors: any) => any;
+  export default flattenColorPalette;
+}
